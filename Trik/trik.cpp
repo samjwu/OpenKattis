a1210005361d050b19228643c55c0ce972878424
samjwu <swm1000@hotmail.com>
2020-10-09 23:36:42 -0600

    Trik problem

---
+++ Trik/trik.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+
+int main() {
+	std::string input;
+	std::cin >> input;
+
+    int pos = 1;
+    
+    for (int i=0; i<input.length(); i++) {
+        switch (input[i]) {
+            case 'A':
+                switch(pos) {
+                    case 1:
+                        pos = 2;
+                        continue;
+                    case 2:
+                        pos = 1;
+                        continue;
+                    case 3:
+                        continue;
+                }
+            case 'B':
+                switch(pos) {
+                    case 1:
+                        continue;
+                    case 2:
+                        pos = 3;
+                        continue;
+                    case 3:
+                        pos = 2;
+                        continue;
+                }
+            case 'C':
+                switch(pos) {
+                    case 1:
+                        pos = 3;
+                        continue;
+                    case 2:
+                        continue;
+                    case 3:
+                        pos = 1;
+                        continue;
+                }
+        }
+    }
+
+	std::cout << pos << std::endl;
+
+	return 0;
+}
