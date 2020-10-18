a3db3bd6368f1062fb7a8c3ae4c547d896268ba8
samjwu <swm1000@hotmail.com>
2020-10-18 16:22:46 -0600

    tarifa

---
+++ Tarifa/tarifa.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int x, n, p;
+    std::cin >> x >> n;
+
+    int ans = x * (n+1);
+    for (int i=0; i<n; i++) {
+        std::cin >> p;
+        ans -= p;
+    }
+
+    std::cout << ans << std::endl;
+    
+    return 0;
+}
