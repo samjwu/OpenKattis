5314d50129d03595b2685d5af984b865db644400
samjwu <swm1000@hotmail.com>
2021-06-09 21:07:55 -0600

    sort

---
+++ Sort Two Numbers/sort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    
+    if (a < b) {
+        std::cout << a << ' ' << b;
+    } else {
+        std::cout << b << ' ' << a;
+    }
+}
