ef69663bf74721c18a3b93331f44768163e43364
samjwu <sjwu@ualberta.ca>
2021-10-21 18:24:06 -0600

    cinema2

---
+++ Cinema Crowds 2/cinema2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    int g, ans = 0;
+    for (int i=0; i<m; i++) {
+        std::cin >> g;
+        if (g <= n) {
+            n -= g;
+        } else {
+            ans = m-i;
+            break;
+        }
+    }
+    std::cout << ans;
+}
