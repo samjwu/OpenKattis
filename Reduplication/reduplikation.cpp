8d57c2b38bf80c1e644a4984b5d6c525fa3a007d
samjwu <22262939+samjwu@users.noreply.github.com>
2024-08-23 17:43:51 -0600

    reduplikation

---
+++ Reduplication/reduplikation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s;
+    int n;
+    std::cin >> s >> n;
+    
+    for (int i = 0; i < n; i++) {
+        std::cout << s;
+    }
+}
